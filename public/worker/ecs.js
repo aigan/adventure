d649f5735d070a2dd5404967e80f01da4f07af70
Jonas Liljegren <jonas@liljegren.org>
2020-05-09 01:53:43 +0200

    Started on dynamic component system

--- public/worker/ecs.js
+++ public/worker/ecs.js
@@ -1,3 +1,98 @@
-const ECS = {
+log('Loading ECS');
+// Taking some concepts from ECS and https://blog.mozvr.com/introducing-ecsy/
+// And stuff from https://github.com/bvalosek/tiny-ecs
+
+
+
+class World {
+  constructor(){
+    // super();
+    this.entity = new Map();
+  }
+  createEntity(){
+    const e = new Entity();
+    this.entity.set(e.id,e);
+    return e;
+  }
+}
+
+class Entity {
+  constructor(){
+    // super();
+    this.id = ++ Entity.cnt;
+  }
   
+  addComponent( Component, values ){
+    // log('should add', this.id, Component.name, values);
+    const c = new Component( values );
+    this[ Component.name ] = c;
+    // log('res', this);
+  }
+  
+}
+Entity.cnt = 0;
+
+class Component {}
+
+class TagComponent extends Component {}
+
+function createComponentClass( def, name ){
+  // log('should create component', name, def);
+
+  if( typeof def === 'string'){
+    def = {value:{type:def}};
+  }
+  
+  let C;
+  if( !Object.keys(def).length ){
+    C = class extends TagComponent{};
+  } else {
+    C = function( values ){
+      // log('init', C.name, 'with', values);
+
+      // Convert singulars
+      if( typeof values === 'string' ){
+        values = {value:values};
+      }
+      
+      // Convert entity values to id
+      if( values.id ){
+        values = {value:values}
+      }
+
+      for( const key in def ){
+        let val = values[key];
+        const attr = def[key];
+        if( val ){
+          const type = attr.type;
+          if( type === 'string' ){}
+          else {
+            if( typeof val === 'string' ){
+              throw `Component ${C.name} ${key} expects entity. Got ${val}`;
+            }
+            val = val.id;
+          }
+        }
+
+
+        // log('set', key, val, attr);
+        this[key] = val;
+      }
+    }
+    C.prototype.schema = def;
+  }
+
+  if (typeof name !== "undefined") {
+    Object.defineProperty(C, "name", { value: name });
+  }
+
+  // log('created comp', C.name );
+  return C;
+}
+
+const ECS = {
+  World,
+  Component,
+  TagComponent,
+  createComponentClass,
 };
--- public/worker/world.js
+++ public/worker/world.js
@@ -2,84 +2,87 @@ log('Loading world');
 // Taking some concepts from ECS and https://blog.mozvr.com/introducing-ecsy/
 // And stuff from https://github.com/bvalosek/tiny-ecs
 
-importScripts('../vendor/ecsy.js'); // ECSY
+importScripts('./ecs.js'); // ECSY
 
 const TComponent = {
-  Referrable: {},
+  // Referrable: {},
   // Parenting: {},
-  InLocation: { value: {default:-1}},
+  InLocation: 'Location',
   // Slot: {
   //   template: {default:''},
   //   name: {default:''},
   // },
-  Labeled: {
-    singular: {default:''},
-    plural: {default:''},
-  },
-  Race: { value: {default:''}},
+  Labeled: 'string',
+  HasRace: 'Race',
 }
 
 const TEntity = {
   Location: {
     components: {
-      Referrable: true,
+      // Referrable: true,
       Labeled: {},
     }
   },
-  HumanRace: {
+  Race: {
     components: {
-      Labeled: {singular:'human'},
+      Labeled: {},
     }
   },
   Human: {
     components: {
-      Race: 'HumanRace',
+      // HasRace: 'HumanRace',
       InLocation: {},
       Labeled: {},
     }
   }
 }
 
+
+const world = new ECS.World; //## <<<-------
+
 const estore = {};
 const c = {};
 
-function define_tag( t ){
-  // log('tag', t);
-  c[t] = class extends ECSY.TagComponent{};
-  Object.defineProperty (c[t], 'name', {value: t});
-}
+// function define_tag( t ){
+//   // log('tag', t);
+//   c[t] = class extends ECS.TagComponent{};
+//   Object.defineProperty (c[t], 'name', {value: t});
+// }
 
 for( const t in TComponent ){
-  if( !Object.keys(TComponent[t]).length){
-    define_tag( t );
-    continue;
-  }
+  // if( !Object.keys(TComponent[t]).length){
+  //   define_tag( t );
+  //   continue;
+  // }
   
   // log('component', t);
-  c[t] = ECSY.createComponentClass(TComponent[t], t)
+  c[t] = ECS.createComponentClass(TComponent[t], t)
 }
 
 // for( const t in TEntity ){}
-
-const world = new ECSY.World; //## <<<-------
 
 function entity_add( t, props ){
   const def = TEntity[t];
   if( !def ) throw `Entity template ${t} missing`;
   if( !props ) props = {};
   const e = world.createEntity();
-  if( !c[t] ) define_tag( t );
+  if( !c[t] ) c[t] = ECS.createComponentClass( {}, t );
+  // log('addComponent', t, c[t]);
   e.addComponent( c[t] );
   const c_def = def.components;
   for( const ct in c_def ){
     const ctval = c_def[ct];
-    log('adding', t, ct, 'with', ctval, props[ct] );
+    // log('entity', sysdesig(e), 'adding', t, ct, 'with', ctval, props[ct] );
     if( ctval === true ){
       e.addComponent( c[ct] );
       continue;
     }
-    if( props[ct] ){
-      Object.assign( ctval, props[ct] );
+    let initvals =  props[ct];
+    if( initvals ){
+      if( typeof initvals === 'string' ){
+        initvals = {value:initvals}
+      }
+      Object.assign( ctval, initvals );
     }
     e.addComponent( c[ct], ctval );
   }
@@ -97,7 +100,7 @@ function sysdesig( entity ){
   id = entity.id;
   let name = entity.name;
   if( !name ){
-    const label_component = entity.getComponent(c.Labeled);
+    const label_component = entity[c.Labeled];
     if( label_component ){
       name = label_component.singular;
     }
@@ -107,95 +110,25 @@ function sysdesig( entity ){
   return `${name}(${id})`;
 }
 
-class Registry extends ECSY.System{
-  execute(){
-    const placedQuery = this.queries.all;
-    
-    placedQuery.added.forEach( entity=>{
-      estore[entity.id] = entity;
-      log('addeded', entity.id);
-    });
-
-    placedQuery.removed.forEach( entity=>{
-      delete estore[entity.id];
-      log('removed', entity.id);
-    });
-  }
-}
-Registry.queries = {
-  all: {
-    components: [c.Referrable],
-    listen: {
-      added: true,
-      removed: true,
-    }
-  }
-}
-
-class SystemPlaced extends ECSY.System{
-  execute(){
-    const placedQuery = this.queries.placed;
-    
-    placedQuery.added.forEach( entity=>{
-      const parent_id = entity.getComponent(c.InLocation).value;
-      log('added', sysdesig(entity), 'to', sysdesig(parent_id));
-    });
-
-    placedQuery.removed.forEach( entity=>{
-      const parent_id = entity.getRemovedComponent(c.InLocation).value;
-      log('removed', sysdesig(entity), 'from', sysdesig(parent_id));
-    });
-  }
-}
-SystemPlaced.queries = {
-  placed: {
-    components: [ c.InLocation ],
-    listen: {
-      added: true,
-      removed: true,
-    }
-  }
-}
-
-world.registerSystem(Registry);
-world.registerSystem( SystemPlaced );
-world.createEntity(); // workaround for avoiding id 0
 
 
-// const lobby = world.createEntity();
-// lobby.addComponent(c.Location);
-// lobby.addComponent(c.Referrable);
-// lobby.addComponent(c.Labeled, {singular:'Lobby'});
+const lobby = entity_add('Location',{Labeled:'Lobby'})
 
-const lobby = entity_add('Location',{Labeled:{singular:'Lobby'}})
-
-
-const desk = world.createEntity();
-desk.name = 'desk';
-desk.addComponent(c.InLocation, {value:lobby.id});
-
-// const human = world.createEntity();
-// human.addComponent(c.Labeled, {singular:'human'});
-// human.addComponent(c.HumanRace);
-
-const human_race = entity_add('HumanRace')
+// const desk = world.createEntity();
+// desk.name = 'desk';
+// desk.addComponent(c.InLocation, lobby);
 
 
-// const npc1 = world.createEntity();
-// npc1.addComponent(c.InLocation, {value:lobby.id});
-// // npc1.addComponent(c.Slot, {template:'HumanRace', name:'Race'});
-// npc1.addComponent(c.Labeled, {singular:'Catalina'});
-// npc1.addComponent(c.Labeled, {singular:'Catlin'});
-// npc1.addComponent(c.Race, {value:human_race.id})
+// const human_race = entity_add('Race', {Labeled:'human'})
 
 const npc1 = entity_add('Human', {
   InLocation:lobby,
-  Labeled: {singular:'Catalina'},
+  Labeled: 'Catalina',
 })
 
-world.execute();
-desk.remove();
-world.execute();
+// world.execute();
+// desk.remove();
+// world.execute();
 
 log( 'estore', npc1 );
 
